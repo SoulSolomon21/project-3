f2bd701eb73ea626032ef3e1ca90929ce76c5583
SoulSolomon21 <99466179+SoulSolomon21@users.noreply.github.com>
2022-05-24 10:05:09 +0300

    Update project3.js

--- project3/project3.js
+++ project3/project3.js
@@ -6,11 +6,14 @@ const deleteBtn = document.getElementById("delete-btn")
 const tabBtn = document.getElementById("tab-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
+//the if statement below checks if the are any leads stored in local storage
+//and if so, they are added to the my leads array and then renderred to the screen
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 } 
 
+// this is our function that renders the leads that are in our leads array onto the screen
 function render(leads) {
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
@@ -23,6 +26,7 @@ function render(leads) {
     ulEl.innerHTML = listItems 
 }
 
+//this event listener gets the value from our input field and pushes it to the myLeads array
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
@@ -30,12 +34,14 @@ inputBtn.addEventListener("click", function () {
     inputEl.value = ""
 })
 
+//this function is for the delete all button, it clears all the values from the myLeads array and then renders the empty array thus showing a clear screen 
 deleteBtn.addEventListener("dblclick", function () {
     localStorage.clear()
     myLeads = []
     render(myLeads)
 })
 
+// the function interacts with the chrome API to access the url of the currently active tab and then add it to our myLeads array 
 tabBtn.addEventListener("click", function () {
         chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
         myLeads.push(tabs[0].url)
